added the corrected calculation of susceptibility in tfim_lanczos_susceptibility_ave.py

Lanczos_method_plot.py
@@ -9,11 +9,11 @@ import matplotlib.pyplot as pl
 import matplotlib.ticker as mtick
 import os
 
-N = 16
+N = 4
 
-os.chdir("../")
-os.chdir('lanczos_diag_data')
-table = np.loadtxt("lanczos_diagonalization data_[4, 4].txt")
+# os.chdir("../")
+os.chdir('lanczos_ave_data')
+table = np.loadtxt("lanczos_ave data_[3, 3] seed from 2 to 99.txt")
 index_arr = table[: ,0]
 h_x_range = table[:, 1]
 GS_energy_val = table[:, 2]
@@ -72,7 +72,8 @@ pl.tick_params('both', length=7, width=2, which='major')
 pl.tick_params('both', length=5, width=2, which='minor')
 pl.gca().yaxis.set_major_formatter(mtick.FormatStrFormatter('%.6f'))
 pl.grid(False)
-pl.yscale("log")
+# pl.yscale("log")
+pl.ylim((-10, 100))
 pl.legend(loc=0, prop={'size': 16}, numpoints=1, scatterpoints=1, ncol=1)
 fig.tight_layout(pad=0.5)
 # pl.savefig("susceptibility plot")

tfim_average.py
@@ -8,20 +8,20 @@ h_x_range = np.linspace(0., 2., 20)
 PBC = True
 h_z = 0.001
 maxiter = 400
-L = [2,2]
+L = [4, 4]
 
 exc_eigenvalues_all = np.zeros((len(seed_range), len(h_x_range)))
 first_excited__exc_energies_all = np.zeros((len(seed_range), len(h_x_range)))
 second_derivative_exc_eigenvalues_all = np.zeros((len(seed_range), len(h_x_range)))
 chi_arr_all = np.zeros((len(seed_range), len(h_x_range)))
 S_SG_arr_all= np.zeros((len(seed_range), len(h_x_range)))
-for seed in seed_range:
+for i, seed in enumerate(seed_range):
     N, h_x_range, exc_eigenvalues, first_excited__exc_energies, second_derivative_exc_eigenvalues, chi_arr, S_SG_arr = lanczos(L, seed, h_x_range, PBC, h_z, maxiter)
-    exc_eigenvalues_all[seed] = exc_eigenvalues
-    first_excited__exc_energies_all[seed] = first_excited__exc_energies
-    second_derivative_exc_eigenvalues_all[seed] = second_derivative_exc_eigenvalues
-    chi_arr_all[seed] = chi_arr
-    S_SG_arr_all[seed] = S_SG_arr
+    exc_eigenvalues_all[i] = exc_eigenvalues
+    first_excited__exc_energies_all[i] = first_excited__exc_energies
+    second_derivative_exc_eigenvalues_all[i] = second_derivative_exc_eigenvalues
+    chi_arr_all[i] = chi_arr
+    S_SG_arr_all[i] = S_SG_arr
 
 exc_eigenvalues_ave = np.mean(exc_eigenvalues_all, axis = 0)
 first_excited__exc_energies_ave = np.mean(first_excited__exc_energies_all, axis = 0)
@@ -41,6 +41,9 @@ os.chdir(output)
 
 outF = open("lanczos_ave data_{size} seed from {init} to {final}.txt".format(size = L, init = seed_range[1], final = seed_range[-1]), 'w')
 for i, h_x in enumerate(h_x_range):
-    outF.write("{index} {h_x_val} {GS_energy_val} {first_excited_energy_val} {second_derivative_energy_val} {susceptibility_val} {structure_factor_val} \n".format(index = i, h_x_val = h_x, GS_energy_val = exc_eigenvalues[i], first_excited_energy_val = first_excited__exc_energies[i], second_derivative_energy_val = second_derivative_exc_eigenvalues[i], susceptibility_val = chi_arr[i], structure_factor_val = S_SG_arr[i]))
+    outF.write("{index} {h_x_val} {GS_energy_val} {first_excited_energy_val} {second_derivative_energy_val} {susceptibility_val} {structure_factor_val} \n".format(index = i, h_x_val = h_x, GS_energy_val = exc_eigenvalues_ave[i], first_excited_energy_val = first_excited__exc_energies_ave[i], second_derivative_energy_val = second_derivative_exc_eigenvalues_ave[i], susceptibility_val = chi_arr_ave[i], structure_factor_val = S_SG_arr_ave[i]))
     # outF.write("{index} {h_x_val} {GS_energy_val} {first_excited_energy_val} {second_derivative_energy_val} {structure_factor_val} \n".format(index = i, h_x_val = h_x, GS_energy_val = exc_eigenvalues[i], first_excited_energy_val = first_excited__exc_energies[i], second_derivative_energy_val = second_derivative_exc_eigenvalues[i], structure_factor_val = S_SG_arr[i]))
 outF.close()
+
+#exit output directory
+os.chdir("../")